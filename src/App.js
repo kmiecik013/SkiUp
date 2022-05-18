6eae2a62d24979e9dd252f5c26b1207c9106392f
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-05-17 23:29:11 -0600

    ski resort api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 import ResortList from './ResortList';
 import Header from './Header';
 import Footer from './Footer';
+import axios from 'axios';
 
 
 export default function App() {
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat={51.0447}&lon={114.0719}&appid={3af3fc922c2fb32e098de9c686e71004}"
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Fernie&APPID=a7a9665aa1abbfd3de98c6c2706e4466"
+
+const url2 = "https://api.weatherunlocked.com/api/current/51.42,116.17?app_id=1c24c23e&app_key=36b68b891fd640ca338c50abeb953566"
+
+const url3 = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=a7a9665aa1abbfd3de98c6c2706e4466"
+
+const url4 = "http://api.weatherunlocked.com/api.weatherunlocked.com/api/resortforecast/999001?app_id=1c24c23e&app_key=a7a9665aa1abbfd3de98c6c2706e4466"
+
+const url5 = "http://api.worldweatheronline.com/premium/v1/ski.ashx?key=f1043f572972409699600438221805&q=45.9237,6.8694&format=json"
+
+
+let [temp, setTemp] = useState("");
+
+useState(() => {
+  axios
+  .get(url5)
+  .then((res) => setTemp(res.data.weather));
+  console.log(temp,"hello here")
+}
+)
+
+  console.log(url, "url here");
+  console.log(url2, "url2 here");
+  console.log(url3, "url3 here");
+  console.log(url4, "url4 here");
+  console.log(url5, "url5 here");
+
+
 
   return (
-    <div>
+    <div>   
       <Header />
+
+        {temp}
 
       <div className ="ski-content"><ResortList /></div> 
       <Footer />
